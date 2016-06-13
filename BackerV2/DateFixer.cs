a0468e8b66b1db76025660c11b5f83e2e9351702
jonathan2266 <jonathan2266@hotmail.be>
2016-06-13 22:56:51 +0200

    added static datefixer

---
+++ BackerV2/DateFixer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BackerV2
+{
+    public static class DateFixer
+    {
+        public static string fix(DateTime date)
+        {
+            string fixedDate;
+            fixedDate = date.ToString();
+            fixedDate = fixedDate.Replace('/', '_');
+            fixedDate = fixedDate.Replace(':', '-');
+            fixedDate = fixedDate.Replace(" ", "__");
+
+            return fixedDate;
+        }
+    }
+}
