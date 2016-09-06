9b9a1ceb6352c957b77ced24a5ec75b1f6d0d1d0
jonathan2266 <jonathan2266@hotmail.be>
2016-09-06 18:03:24 +0200

    improved logger

---
+++ BackerV2/Logger/Logger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Concurrent;
+
+namespace BackerV2
+{
+    abstract public class Logger : Ilogger
+    {
+        protected bool isFunctional;
+        protected ConcurrentQueue<string> writeBuffer = new ConcurrentQueue<string>();
+
+        public abstract void log(string text);
+
+        public abstract void stopLogging();
+    }
+}
